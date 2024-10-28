df6f614d169bfebac5e6406b4f275dd0b71495d4
TymoteuszKaczynski <tymek.kaczynski@gmail.com>
2024-10-28 16:58:48 +0100

    message

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -1,8 +1,33 @@
 #pragma once
 
 #include "Resource.hpp"
+#include <iostream>
 
-class ResourceManager
-{
-    // Twoja implementacja tutaj
+class ResourceManager {
+private:
+    Resource* res;
+
+public:
+    ResourceManager() : res(new Resource()) {
+        }
+
+    ~ResourceManager() {
+        delete res;  
+    }
+
+    ResourceManager(const ResourceManager& other) : res(new Resource(*other.res)) {
+    }
+
+    ResourceManager& operator=(const ResourceManager& other) {
+        if (this != &other) {
+            delete res; 
+            res = new Resource(*other.res);  
+        }
+        
+        return *this;
+    }
+
+    double get() const {
+         return (*res).get();
+    }
 };
